6b84e10c06738e8a98669c17a951614759f5ac29
hanyusok <hanyusok@gmail.com>
2023-10-03 10:17:40 +0900

    buffer error??

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,68 +1,63 @@
-<script>
+<script setup>
 import fb from "node-firebird";
-import { reactive, computed, ref } from "vue";
-// import { buffer } from "buffer";
+import {
+  reactive,
+  computed,
+  ref,
+  onMounted,
+  onUpdated,
+  onBeforeMount,
+} from "vue";
+import { Buffer } from "buffer";
 
-export default {
-  setup() {
-    const fbResult = ref([]);
-    const sql = ref("");
-    const options = {
-      // host :  '192.168.0.13',
-      host: "127.0.0.1",
-      port: 3050,
-      //database: databaseStr,
-      database:
-        "/Library/Frameworks/firebird.framework/Versions/A/Resources/examples/empbuild/employee.fdb",
-      //database :  'C:\\Program Files\\Firebird\\Firebird_2_5\\examples\\empbuild\\EMPLOYEE.FDB',
-      // database :  'C:\\Mts3\\Db\\MTSDB.FDB',
-      user: "SYSDBA",
-      password: "masterkey",
-      lowercase_keys: false,
-      role: null,
-      pageSize: 4096,
-    };
-    return {
-      fbResult,
-      options,
-      sql,
-    };
-  },
-  created() {
-    console.log("created:");
-  },
-  mounted() {
-    console.log("mounted");
-  },
-  onMounted() {
-    console.log("before fb contact");
-    fb.attach(options, (err, db) => {
+const fbResult = ref([]);
+const sql = ref("");
+const options = {
+  // host :  '192.168.0.13',
+  host: "127.0.0.1",
+  port: 3050,
+  //database: databaseStr,
+  database:
+    "/Library/Frameworks/firebird.framework/Versions/A/Resources/examples/empbuild/employee.fdb",
+  //database :  'C:\\Program Files\\Firebird\\Firebird_2_5\\examples\\empbuild\\EMPLOYEE.FDB',
+  // database :  'C:\\Mts3\\Db\\MTSDB.FDB',
+  user: "SYSDBA",
+  password: "masterkey",
+  lowercase_keys: false,
+  role: null,
+  pageSize: 4096,
+};
+
+onMounted(() => {
+  console.log("mounting...");
+
+  fb.attach(options, (err, db) => {
+    if (err) {
+      console.error("Error connecting to the database: ", err);
+      return;
+    }
+
+    sql.value = "SELECT * FROM EMPLOYEE";
+
+    db.query(slq, (err, result) => {
       if (err) {
-        console.error("Error connecting to the database: ", err);
+        console.error("Error executing the query: ", err);
         return;
       }
+      fbResult.value = result;
 
-      sql.value = "SELECT * FROM EMPLOYEE";
-
-      db.query(slq, (err, result) => {
-        if (err) {
-          console.error("Error executing the query: ", err);
-          return;
-        }
-        fbResult.value = result;
-
-        db.detach();
-      });
+      db.detach();
     });
-  },
-};
-// };
+  });
+});
 </script>
 
 <template>
   <h1>home</h1>
-  <h4>Result:</h4>
+  <h4>Options:</h4>
+  <p>{{ options.value }}</p>
   <!-- <button @click="getfbData">get data</button> -->
   <!-- <div v-bind="fbResult"></div> -->
-  <p>{{ fbResult }}</p>
+  <p>Results:</p>
+  <p>{{ fbResult.value }}</p>
 </template>
